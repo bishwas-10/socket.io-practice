d2b781958ffed41c18b437a2db6f9e5abe17c6c6
bishwas-10 <120932783+bishwas-10@users.noreply.github.com>
2023-12-28 14:03:49 +0545

    basic setup done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React,{useEffect} from 'react';
+import { io } from 'socket.io-client';
 
-
+const socket = io('http://localhost:4000');
 function App() {
+const userData = {
+  name:"bishwas",
+  section:"pmc sec A"
+}
+const joinRoom=()=>{
+  socket.emit('join_room',userData);
+}
+
+  useEffect(()=>{
+    joinRoom();
+  },[])
+
   return (
-   <div>hello scoket.io</div>
+   <div className="text-red-400">hello scoket.io</div>
   );
 }
 
--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,39 @@
 import express from "express";
-
-import http from "http"
-
+import http from "http";
 import cors from "cors";
+import { Server } from "socket.io";
+
 const app = express();
+
 app.use(cors()); // Add cors middleware
 
 const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+
+io.on("connection", (socket)=>{
+    console.log(`connection established with user ${socket.id}`),
+   socket.on("join_room",(data)=>{
+    const {name,section } = data;
+    socket.join(section);
+   
+    let created_time = new Date();
+    socket.to(section).emit('receive_message',{
+        message:`${name} has just joined the room `,
+        username:name,
+        created_time   
+    });
+    socket.emit('receive_message',{
+        message:`welcome ${name}`,
+        created_time
+    })
+   }) 
+});
 
 server.listen(4000, () => console.log("server listening on port 4000"));
