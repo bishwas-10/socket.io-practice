a27af53ef732651143a4dcce36b7f748ff8ee04c
bishwas-10 <120932783+bishwas-10@users.noreply.github.com>
2023-12-28 15:10:21 +0545

    created a form page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,19 @@
 import React,{useEffect} from 'react';
 import { io } from 'socket.io-client';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import ChatPage from './components/ChatPage';
+import Home from './components/Home';
 
-const socket = io('http://localhost:4000');
 function App() {
-const userData = {
-  name:"bishwas",
-  section:"pmc sec A"
-}
-const joinRoom=()=>{
-  socket.emit('join_room',userData);
-}
-
-  useEffect(()=>{
-    joinRoom();
-  },[])
 
   return (
-   <div className="text-red-400">hello scoket.io</div>
+   <Router>
+    <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/chat' element={<ChatPage/>}/>
+
+    </Routes>
+   </Router>
   );
 }
 
---
+++ client/src/components/Home.tsx
@@ -0,0 +1,63 @@
+import React, { ChangeEvent, useState } from "react";
+
+const Home = () => {
+  const [name, setName] = useState<string>("");
+  const [room, setRoom] = useState<string>("");
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(name, room);
+  };
+  const handleSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setRoom(e.target.value);
+  };
+  return (
+    <div className="bg-gray-900 w-screen h-screen flex flex-col items-center justify-center text-white">
+      <form
+        action="submit"
+        onSubmit={handleSubmit}
+        className="py-6 px-4 w-96   rounded-sm bg-gray-800 flex flex-col gap-4"
+      >
+        <p className="text-center text-2xl">Welcome to Simple Chat App</p>
+
+        <div className="flex flex-col items-center gap-4 text-black">
+          <div className="flex gap-4 items-center">
+            <label htmlFor="username" className="text-white">
+              Username
+            </label>
+            <input
+              type="text"
+              minLength={6}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              name="username"
+              placeholder="your-username"
+              className="p-2  focus:outline-none"
+            />
+          </div>
+
+          <select
+            value={room}
+            onChange={handleSelectChange}
+            name="room"
+            id="chatroom"
+            className="w-40 focus:outline-none"
+          >
+            <option value="JavaScript">JavaScript</option>
+            <option value="Python">Python</option>
+            <option value="C++">C++</option>
+            <option value="Java">Java</option>
+            <option value="Php">Php</option>
+          </select>
+          <button
+            type="submit"
+            className=" w-20 text-white border-2 p-2 border-gray-500 rounded-lg"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Home;
