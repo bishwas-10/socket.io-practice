3e426932b3fafe1c63f1bbda865bf9a30b7d0fd2
bishwas-10 <120932783+bishwas-10@users.noreply.github.com>
2023-12-29 19:40:44 +0545

    added chat bubble

--- client/src/components/ChatBody.tsx
+++ client/src/components/ChatBody.tsx
@@ -14,22 +14,41 @@ const ChatBody = ({ message }: { message: ChatMessage[] }) => {
           Leave Chat
         </button>
       </div>
-      <div className="relative top-16">
-        {message.map((message: ChatMessage, index: number) =>
+      <div className="relative top-16 w-full md:px-8">
+        {/* {message.map((message: ChatMessage, index: number) =>
           message.username === localStorage.getItem("username") ? (
             <div key={index}
-            className=""
+            className=" w-[60%] flex flex-col gap-1 p-2 items-end"
             >
+              <p>You</p>
+              <p className="px-3 py-2 rounded-lg w-max bg-green-400">lorem</p>
+            </div>
+          ) : (
+            <div key={index} className="w-[60%] flex flex-col gap-1 p-2 items-start">
               <p>{message.username}</p>
               <p>{message.message}</p>
             </div>
-          ) : (
-            <div key={index}>
-              <p>You</p>
-              <p>{message.message}</p>
-            </div>
           )
-        )}
+        )} */}
+        <div className=" w-[60%] ml-auto flex flex-col gap-1 p-2  items-end">
+          <div className="flex flex-row gap-2 items-center text-gray-600">
+           
+            <p>You</p>
+            <span className="text-xs ">12:30</span>
+          </div>
+          <p className="px-3 py-2 rounded-lg max-w-full bg-green-400 text-right">
+            asndasnd
+          </p>
+        </div>
+
+        <div className="w-[60%] mr-auto flex flex-col gap-1 p-2 items-start">
+        <div className="flex flex-row gap-2 items-baseline text-gray-600">
+           
+           <p>You</p>
+           <span className="text-xs ">12:30</span>
+         </div>
+          <p className="px-3 py-2 rounded-lg max-w-full bg-red-400">saddsd</p>
+        </div>
       </div>
     </div>
   );
